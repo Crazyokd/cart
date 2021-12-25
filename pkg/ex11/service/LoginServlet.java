ae2d7b3a3cf763d56047b5e1918916355dc639b9
Rekord <1324596506@qq.com>
2021-12-26 00:27:26 +0800

    finish LoginServlet.java

---
+++ pkg/ex11/service/LoginServlet.java
@@ -0,0 +1,36 @@
+package pkg.ex11.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pkg.ex11.domain.User;
+import pkg.ex11.service.impl.BusinessServiceImpl;
+
+public class LoginServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		BusinessServiceImpl service = new BusinessServiceImpl();
+		User user = service.loginUser(username, password);
+		if(user == null){
+			request.setAttribute("message", "用户名或密码不正确");
+			request.getRequestDispatcher("/static/message.jsp").forward(request, response);
+			return;
+		}
+		request.getSession().setAttribute("user", user);
+		request.getRequestDispatcher("/static/login.jsp").forward(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
