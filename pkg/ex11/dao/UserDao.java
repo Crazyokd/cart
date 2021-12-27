08a73169f393d1f65667abd3bd613e2c11035720
Rekord <1324596506@qq.com>
2021-12-27 21:19:42 +0800

    形参遵循命名规范（强迫症又犯了？）

--- pkg/ex11/dao/UserDao.java
+++ pkg/ex11/dao/UserDao.java
@@ -5,6 +5,6 @@ import pkg.ex11.domain.User;
 public interface UserDao {
 
 	void add(User user);
-	User search(String id);
+	User search(String username);
 	User search(String username, String password);
 }
